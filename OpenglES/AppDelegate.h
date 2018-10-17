14c660dd20eb26d3041f89fb86848707c92a9a83
loneswordman <javandoc1985@gmail.com>
2018-10-17 15:15:32 +0800

    update

---
+++ OpenglES/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  OpenglES
+//
+//  Created by developer on 2018/10/17.
+//  Copyright © 2018 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ OpenglES/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenglES
+//
+//  Created by developer on 2018/10/17.
+//  Copyright © 2018 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
